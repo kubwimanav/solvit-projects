e2a894086295b86f6b28881a779cbbe88fd6a03b
Mahoro <lionmill3@gmail.com>
2025-07-17 01:30:19 +0200

    updated the author js

---
+++ display.js
@@ -0,0 +1,7 @@
+ const userBlock = document.getElementById('user-block')
+ const adminBlock = document.getElementById('admin-block')
+
+function userdata(){
+    const user  = JSON.parse(localStorage.getItem(users))
+
+}
