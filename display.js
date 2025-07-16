06f32e3451c4ba5bd04c3071c4d17a80bec4412c
Mahoro <lionmill3@gmail.com>
2025-07-17 01:48:53 +0200

    removed unnecessary files

--- display.js
+++
@@ -1,7 +0,0 @@
- const userBlock = document.getElementById('user-block')
- const adminBlock = document.getElementById('admin-block')
-
-function userdata(){
-    const user  = JSON.parse(localStorage.getItem(users))
-
-}
